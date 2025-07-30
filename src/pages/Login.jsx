9c25038a52fa01d7b703dff68449118cf21b4fe7
João Luccas <joao.marques@grupoperfiledu.com.br>
2025-07-30 17:05:37 -0300

    update: assets folder

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -15,7 +15,7 @@ function LoginScreen() {
                         className={style.header}>
                             <img
                                 className={style.logo} 
-                                src="src/assets/logoBranca.png" 
+                                src="src/assets/logos/logoBranca.png" 
                                 alt="Logo Perfil Branca"/>
                             <h1
                                 className={style.title}>
